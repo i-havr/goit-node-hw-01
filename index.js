bb863f0046f6b0f0e7b8a5543c69ae9730003290
i-havr <i.havrylov@gmail.com>
2023-03-03 08:51:54 +0200

    add fix

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const { Command } = require("commander");
+const { Command } = require('commander');
 
 const {
   listContacts,
   getContactById,
   removeContact,
   addContact,
-} = require("./contacts");
+} = require('./contacts');
 
 const program = new Command();
 
 program
-  .option("-a, --action <type>", "choose action")
-  .option("-i, --id <type>", "user id")
-  .option("-n, --name <type>", "user name")
-  .option("-e, --email <type>", "user email")
-  .option("-p, --phone <type>", "user phone");
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
 program.parse(process.argv);
 
@@ -22,24 +22,24 @@ const argv = program.opts();
 
 function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
-    case "list":
+    case 'list':
       listContacts();
       break;
 
-    case "get":
+    case 'get':
       getContactById(id);
       break;
 
-    case "add":
+    case 'add':
       addContact(name, email, phone);
       break;
 
-    case "remove":
+    case 'remove':
       removeContact(id);
       break;
 
     default:
-      console.warn("\x1B[31m Unknown action type!");
+      console.warn('\x1B[31m Unknown action type!');
   }
 }
 
